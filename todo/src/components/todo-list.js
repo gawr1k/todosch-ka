b4a88339a465ed32b0ef48f19871acb9c4c95a80
Гасим Мурасов <gasimmurasov@gmail.com>
2023-09-07 19:21:22 +0500

    1.3.9

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -1,14 +1,24 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
-const TodoList = () => {
 
+
+
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+        return (
+            <li>
+                <TodoListItem {...item}/>
+            </li>
+        )
+    })
 
     return (
         <ul>
-            <li><TodoListItem label = 'Drink Coffee'/></li>
-            <li><TodoListItem label = 'Build React App' important/></li>
+            {elements}
         </ul>
     );
 };
 
 export default TodoList;
+
--- todo/src/index.js
+++ todo/src/index.js
@@ -9,11 +9,17 @@ import TodoList from "./components/todo-list";
 
 
 const App = () => {
+
+    const todoData = [
+        {label: 'Drink Cofee', important:false},
+        {label: 'Make Awesome App', important:true},
+        {label: 'Have a lunch', important:false}
+    ]
     return (
         <div>
             <AppHeader/>
             <SearchPanel/>
-            <TodoList/>
+            <TodoList todos={todoData}/>
         </div>
     );
 };
