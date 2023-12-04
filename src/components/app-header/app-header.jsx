97146e4a019f60561e39ce44931c087def087464
Гасим Мурасов <gasimmurasov@gmail.com>
2023-12-04 17:51:05 +0500

    added a timer layout

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const AppHeader = () => {
+  return (
+    <header className="header">
+      <h1>todos</h1>
+    </header>
+  );
+};
+
+
+export default AppHeader;
---
+++ src/components/new-task-form/new-task-form.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable quotes */
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class NewTaskForm extends Component {
+  state = {
+    label: "",
+    minutes: "",
+    seconds: "",
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      label: event.target.value.trimStart(),
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onAdded(
+      this.state.label,
+      this.state.minutes,
+      this.state.seconds
+    );
+    this.setState({
+      label: "",
+      minutes: "",
+      seconds: "",
+    });
+  };
+
+  onChangeMinute = (event) => {
+    this.setState({
+      minutes: Number(event.target.value),
+    });
+  };
+
+  onChangeSecond = (event) => {
+    this.setState({
+      seconds: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <header>
+        <h1>todos</h1>
+        <form className="new-todo-form" onSubmit={this.onSubmit}>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+            required
+            onInput={this.onInputChange}
+            value={this.state.label}
+          ></input>
+          <input className="new-todo-form__timer" placeholder="Min" autoFocus />
+          <input className="new-todo-form__timer" placeholder="Sec" autoFocus />
+
+          <button type="submit" />
+        </form>
+      </header>
+    );
+  }
+}
+
+NewTaskForm.defaultProps = {
+  onAdded: () => {},
+};
+
+NewTaskForm.propTypes = {
+  onAdded: PropTypes.func,
+};
